4404533ec8fd4fadf4b602c5621b4df642f08e1a
hhsscioly <54913666+hhsscioly@users.noreply.github.com>
2021-11-13 12:38:00 -0500

    Add files via upload

--- schedule/calendar-events.js
+++ schedule/calendar-events.js
@@ -1,20 +1,42 @@
 /*
-  format: [["DATE CODE","EVENT INFO",TRUE/FALSE],...]
-  TRUE/FALSE refers to whether or not an event goes on list schedule (joke items like "Wish Matt a happy birthday :)" would have false for that value)
+format: [["DATE CODE", "EVENT INFO", "REPEAT CODE"],...]
+DATE CODE = "m-d-y"
+EVENT INFO is what goes in the table cell in the calendar and under the event name in the list. It is raw HTML code, so plain text or tags can be used
+  
+REPEAT CODE key: 
+"0" = never repeat, otherwise "n-m-d-y" 
+n = number of repeats, including the first occurrence
+m = how many months
+d = how many days, y = how many years
+"5-1-0-0" would be repeat once every month for five months
+"4-0-7-0" would repeat every week for four weeks
+If an event's repeat code isn't "0" it won't be on the list-- repeating events aren't listed, just on the calendar. To exclude a non-repeating even from the list, use "0-0-0-0"
 */
 
 var events = [
-  ["9-10-2019","<b>First meeting</b><br>",true],
-  ["10-31-2019","Spooky Time",false],
-  ["11-11-2019","Wish Matt a happy birthday :)",false],
-  ["12-11-2019","<b>Pizza Resource Night</b>",true],
-  ["12-14-2019","<b>Portage Invitational</b>",true],
-  ["1-18-2020","<b>Haslett Invitational</b><br><span style='color: red'>TO WEATHER, MOVED TO 2-29</span>",false],
-  ["2-1-2020","<b>WOSO Invitional</b><br>If we do it, only one team will go",true],
-  ["2-7-2020","<b>Frankenmuth Invitational</b><br>Staying night in hotel Friday night, competition Saturday",true],
-  ["2-8-2020","<b>Frankenmuth Invitational</b><br>Staying night in hotel Friday night, competition Saturday",true],
-  ["2-22-2020","<b>Allendale Invitational</b>",true],
-  ["2-29-2020","<b>Haslett Invitational</b>",true],
-  ["3-21-2020","<b>WMU - Regionals</b>",true],
-  ["4-25-2020","<b>States</b><br>If we make it top 17 kids go. You can leave early after your events for prom.",true]
+  ["9-10-2019","<b>First meeting</b><br>", "0"],
+  ["10-31-2019","Spooky Time","0"],
+  ["11-11-2019","Wish Matt a happy birthday :)","80-0-0-1"],
+  ["12-11-2019","<b>Pizza Resource Night</b>","0"],
+  ["12-14-2019","<b>Portage Invitational</b>","0"],
+  ["1-18-2020","<b>Haslett Invitational</b><br><span style='color: red'>TO WEATHER, MOVED TO 2-29</span>","0"],
+  ["2-1-2020","<b>WOSO Invitional</b><br>If we do it, only one team will go","0"],
+  ["2-7-2020","<b>Frankenmuth Invitational</b><br>Staying night in hotel Friday night, competition Saturday","0"],
+  ["2-8-2020","<b>Frankenmuth Invitational</b><br>Staying night in hotel Friday night, competition Saturday","0"],
+  ["2-22-2020","<b>Allendale Invitational</b>","0"],
+  ["2-29-2020","<b>Haslett Invitational</b>","0"],
+  ["3-21-2020","<b>WMU - Regionals</b>","0"],
+  ["4-25-2020","<b>States</b><br>If we make it top 17 kids go. You can leave early after your events for prom.","0"],
+
+  ["1-29-2022", "<b>Haslett Invitational</b>", "0"],
+  ["2-12-2022", "<b>Frankenmuth Invitational</b><br>Leave at 5:30 AM and return at 8:30 PM. Not spending the night", "0"],
+  ["3-20-2022", "<b>Regionals at WMU</b>", "0"],
+  ["5-3-2022", "<b>States</b>", "0"],
+  ["11-13-2021", "Practice 9AM-12PM", "2-0-7-0"],
+  ["12-4-2021", "Practice 9AM-12PM", "0-0-0-0"],
+  ["1-8-2022", "Practice 9AM-12PM", "2-0-7-0"],
+  ["11-18-2021", "Practice 3PM-5PM", "0-0-0-0"],
+  ["12-2-2021", "Practice 3PM-5PM", "16-0-7-0"]
 ];
+
+// having old events on the calendar keeps a historical record but is definitely not a necessity. Delete if there are so many it gets confusing
--- schedule/calendar-script.js
+++ schedule/calendar-script.js
@@ -1,55 +1,198 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-monthLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-var date = new Date();
-var position = [date.getMonth(), date.getFullYear()];
-calendarHere.innerHTML = `<table style="border: none; width: 100%;">
-<tbody>
-<tr>
-<th style="border: none; cursor: pointer;"><h1 onclick="position[0] = position[0] - 1;if(position[0] == -1) {position[0] = 11;position[1] = position[1] - 1;}makeCalendar();"><<</h1></th>
-<th style="border: none;" colspan="5"><h1 id="month"></h1></th>
-<th style="border: none; cursor: pointer;"><h1 onclick="position[0] = position[0] + 1;if(position[0] == 12) {position[0] = 0;position[1] = position[1] + 1;}makeCalendar();">>></h1></th>
-</tr>
-</tbody>
-</table>
-<div id="realCalendar"></div>`;
-function makeCalendar() {
-  var calendar = `<table>
-<tbody>
-<tr>
-<th>Sunday</th>\n<th>Monday</th>\n<th>Tuesday</th>\n<th>Wednesday</th>\n<th>Thursday</th>\n<th>Friday</th>\n<th>Saturday</th>\n</tr>\n`;
-  month.innerHTML = months[position[0]] + " " + position[1].toString();
-  var firstOfMonth = new Date(position[1],position[0],1);
-  var firstDay = firstOfMonth.getDay() + 1;
-  var nOfDays = monthLengths[position[0]];
-  if (position[0] == 1 && Number.isInteger(position[1] / 4)) {
-    nOfDays++;
+var monthLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function parseCode(str) {
+  var out = [0,0,0];
+  for(var i = 0; i < 2; i++) {
+    out[i] = parseInt(str.substring(0, str.indexOf("-")));
+    str = str.substring(str.indexOf("-") + 1, str.length);
   }
-  if(firstDay > 1) {
-    calendar = calendar + "<tr>\n<td colspan='"+(firstDay - 1).toString()+"' style='color: transparent;'>"+months[position[0]-1]+"</td>";
-  }
-  var x = firstDay;
-  for (i = 1; i <= nOfDays; i++) {
-    if (x == 1) {
-     calendar = calendar + `<tr> \n`;
+  out[i] = parseInt(str);
+  return out;
+}
+
+for(var i = 0; i < events.length; i++) {
+  if(events[i][2][0] != "0") {
+    var repeat = events[i][2];
+    var n = parseInt(repeat.substring(0, repeat.indexOf("-")));
+    repeat = repeat.substring(repeat.indexOf("-") + 1, repeat.length);
+    var dMDY = parseCode(repeat);
+    var MDY1 = parseCode(events[i][0]);
+    for(var j = 0; j < n; j++) {
+      var MDY2 = [MDY1[0] + j * dMDY[0], MDY1[1] + j * dMDY[1], MDY1[2] + j * dMDY[2]];
+      while(MDY2[0] > 12) {
+        MDY2[0] -= 12;
+        MDY2[2]++;
+      }
+      while(MDY2[1] > monthLengths[MDY2[0]-1] + (MDY2[0] == 2 && Number.isInteger(MDY2[2] / 4) ? 1 : 0) ) {
+        MDY2[1] -= monthLengths[MDY2[0]-1] + (MDY2[0] == 2 && Number.isInteger(MDY2[2] / 4) ? 1 : 0);
+        MDY2[0]++;
+        if(MDY2[0] == 13) {
+          MDY2[0] = 1;
+          MDY2[2]++;
+        }
+      }
+      events.push([MDY2[0] + "-" + MDY2[1] + "-" + MDY2[2], events[i][1], "0"]);
     }
-    calendar = calendar + `<td>\n` + i.toString();
-    var dateCode = (position[0] + 1).toString() + "-" + i.toString() + "-" + position[1].toString();
-    for (var j = 0; j < events.length; j++) {
-      if (dateCode == events[j][0]) {
-        calendar = calendar + "<br>"+events[j][1];
+    events.splice(i,1);
+    i--;
+  }
+}
+
+document.head.innerHTML = `
+<style>
+.calendarClass table {
+  table-layout: fixed;
+  width: 100%;
+}
+.bordered {
+  border: 1px solid black;
+  width: 14.2857%;
+  text-align: left;
+  vertical-align: text-top;
+  padding: .3em;
+}
+td.bordered {
+  height: 5em;
+}
+th.bordered {
+  text-align: center;
+}
+table, th, td {
+  border-collapse: collapse;
+}
+@media only screen {
+  .today {
+    border: 2px solid red !important;
+  }
+  .bordered {
+    border: 1px solid black;
+  }
+  .arrow {
+    cursor: pointer;
+  }
+}
+@media only print {
+  .today {
+    border: 2px solid black !important;
+  }
+  .bordered {
+    border: 2px solid black;
+  }
+  .arrow {
+    color: transparent;
+  }
+}
+</style>
+` + document.head.innerHTML;
+
+var date = new Date();
+function makeCalendar(calNum) {
+  var calDiv = document.getElementsByClassName("calendarClass")[calNum];
+  var initMonth = true;
+  var startMonth = calDiv.getElementsByClassName("calMonth")[0];
+  if(startMonth != undefined) {
+    if(parseInt(startMonth.innerHTML) > 0 && parseInt(startMonth.innerHTML) < 13) {
+      initMonth = false;
+    }
+  }
+  if(initMonth) {
+    var calMonth = document.createElement("div");
+    calMonth.className = "calMonth";
+    calMonth.style.display = "none";
+    calMonth.innerHTML = date.getMonth() + 1;
+    calDiv.appendChild(calMonth);
+  }
+  var initYear = true;
+  var startYear = calDiv.getElementsByClassName("calYear")[0];
+  if(startYear != undefined) {
+    if(parseInt(startYear.innerHTML) > -271815) {
+      initYear = false;
+    }
+  }
+  if(initYear) {
+    var calYear = document.createElement("div");
+    calYear.className = "calYear";
+    calYear.style.display = "none";
+    calYear.innerHTML = date.getFullYear();
+    calDiv.appendChild(calYear);
+  }
+  var calMonth = calDiv.getElementsByClassName("calMonth")[0];
+  var calYear = calDiv.getElementsByClassName("calYear")[0];
+  calDiv.innerHTML = "";
+  calDiv.appendChild(calMonth);
+  calDiv.appendChild(calYear);
+  var position = [parseInt(calDiv.getElementsByClassName("calMonth")[0].innerHTML) - 1, parseInt(calDiv.getElementsByClassName("calYear")[0].innerHTML)];
+  var calendar = `
+<table width='100%'>
+<tbody>
+<tr>
+<td colspan='7' style='font-size: 2em; text-align: center; padding: 1em;'>
+<span style='float: left; padding-left: 2em;' class='arrow' onclick='move(` + calNum + `, -1)'><<</span>
+<b>` + months[position[0]] + ` ` + (position[1] > 0 ? position[1] : Math.abs(position[1]) + " B.C.E.") + `</b>
+<span style='float: right; padding-right: 2em;' class='arrow' onclick='move(` + calNum + `, 1)'>>></span>
+</td>
+</tr>
+<tr>
+<th class="bordered">Sunday</th>
+<th class="bordered">Monday</th>
+<th class="bordered">Tuesday</th>
+<th class="bordered">Wednesday</th>
+<th class="bordered">Thursday</th>
+<th class="bordered">Friday</th>
+<th class="bordered">Saturday</th>
+</tr>
+`;
+  var dayOfWeek = (new Date(position[1], position[0], 1)).getDay();
+  for(var day = 1; day <= monthLengths[position[0]] + (position[0] == 1 && Number.isInteger(position[1]/4) ? 1 : 0); day++) {
+    if(day == 1 && dayOfWeek != 0) {
+      calendar += `
+<tr>
+<td class='bordered' colspan='` + dayOfWeek + `' style='color: transparent'>.</td>
+`;
+    }
+    if(dayOfWeek == 0) {calendar += `<tr>`;}
+    calendar += `<td class='bordered ` + (date.getDate()==day && date.getMonth()==position[0] && date.getFullYear()==position[1] ? `today` : ``) + `'>` + day;
+    var dateCode = (position[0] + 1) + "-" + day.toString() + "-" + position[1];
+    for(var i = 0; i < events.length; i++) {
+      if(dateCode == events[i][0]) {
+        calendar += `<p style='padding: 0; margin: 0; margin-left: 10px; text-indent: -10px; padding-bottom: 5px;'>` + events[i][1] + `</p>`;
       }
     }
-    calendar = calendar + `</td>\n`;
-    x++;
-    if (x == 8) {
-      calendar = calendar + "</tr>\n";
-      x = 1;
+    calendar += `</td>`;
+    dayOfWeek++;
+    if(dayOfWeek == 7) {
+      calendar += `</tr>`;
+      dayOfWeek = 0;
     }
   }
-  if(x != 1) {
-    calendar = calendar + "</tr> \n";
+  if(dayOfWeek != 0) {
+    calendar += `</tr>`;
   }
-  calendar = calendar + "</tbody>\n</table>\n";
-  realCalendar.innerHTML = calendar;
+  calDiv.innerHTML += calendar + `
+</tbody>
+</table>
+`;
 }
-makeCalendar();
+
+function move(calNum, direction) {
+  var calDiv = document.getElementsByClassName("calendarClass")[calNum];
+  var position = [parseInt(calDiv.getElementsByClassName("calMonth")[0].innerHTML) - 1, parseInt(calDiv.getElementsByClassName("calYear")[0].innerHTML)];
+  position[0] += direction;
+  if(position[0] == -1) {
+    position[0] = 11;
+    position[1]--;
+  }
+  if(position[0] == 12) {
+    position[0] = 0;
+    position[1]++;
+  }
+  calDiv.getElementsByClassName("calMonth")[0].innerHTML = position[0] + 1;
+  calDiv.getElementsByClassName("calYear")[0].innerHTML = position[1];
+  makeCalendar(calNum);
+}
+
+var calendars = document.getElementsByClassName("calendarClass");
+for(var i = 0; i < calendars.length; i++) {
+  makeCalendar(i);
+}
