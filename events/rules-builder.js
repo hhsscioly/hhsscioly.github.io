5ce5860a3b9c7e8642756316fe63a7dfbc3eb5fc
hhsscioly <54913666+hhsscioly@users.noreply.github.com>
2021-11-15 03:46:29 -0500

    Delete events directory

--- events/rules-builder.js
+++
@@ -1,7 +0,0 @@
-var url = window.location.href;
-url = url.substring(0, url.lastIndexOf("/"));
-url = url.substring(0, url.lastIndexOf("/"));
-name = url.substring(url.lastIndexOf("/") + 1);
-url = url.substring(0, url.lastIndexOf("/") + 1);
-url += "rules/" + name + "-rules.pdf";
-window.location.replace(url);
